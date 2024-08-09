74014f38de7ea15cefe0d089dd94b534d78e3abe
= <=>
2024-08-09 11:25:14 +0530

    switch

---
+++ switch.js
@@ -0,0 +1,14 @@
+let fav_cofee = "latte";
+switch (fav_cofee) {
+  case "espresso":
+    console.log("fav_cofee is espresso");
+    break;
+  case "cappuccino":
+    console.log("fav_cofee is cappuccino");
+    break;
+  case "latte":
+    console.log("fav_cofee is latte");
+    
+  case "mocha":
+    console.log("fav_cofee is mocha");
+}
